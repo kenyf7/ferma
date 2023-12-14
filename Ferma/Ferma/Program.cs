73c24259faec243d93b309bb60a9c69632f9ad00
kenyf7 <115103255+kenyf7@users.noreply.github.com>
2023-12-14 22:36:35 +0300

    Пифагор

---
+++ Ferma/Ferma/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Теорема Пифагора");
+
+        Console.Write("Первый катет: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Второй катет: ");
+        double b = double.Parse(Console.ReadLine());
+
+        double cSquared = a * a + b * b;
+
+        Console.WriteLine($"Квадрат гипотенузы: {cSquared}");
+        Console.WriteLine($"Сумма квадратов катетов: {a * a + b * b}");
+
+        if (cSquared == a * a + b * b)
+        {
+            Console.WriteLine("Теорема подтверждена");
+        }
+        else
+        {
+            Console.WriteLine("Теорема не выполняется");
+        }
+    }
+}
